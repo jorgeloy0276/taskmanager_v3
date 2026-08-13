cec513452cc41f4199b1478f8242ec086ed14fc4
jorgeloy0276 <jorgeloy0276@gmail.com>
2026-08-12 20:59:50 -0500

    Hacer robusta la ruta del certificado SSL de MySQL

--- config.py
+++ config.py
@@ -3,13 +3,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def _resolver_ca():
+    ruta = os.getenv('MYSQL_SSL_CA')
+    if ruta and os.path.isfile(ruta):
+        return ruta
+    return os.path.join(BASE_DIR, 'app', 'static', 'ssl', 'ca.pem')
+
+
 class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
     MYSQL_PORT = os.getenv('MYSQL_PORT', 3306)
     MYSQL_DB = os.getenv('MYSQL_DB', 'task_manager')
-    MYSQL_SSL_CA = os.getenv('MYSQL_SSL_CA', '')
+    MYSQL_SSL_CA = _resolver_ca()
     SECRET_KEY = os.getenv('SECRET_KEY', 'default')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
